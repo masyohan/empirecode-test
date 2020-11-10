7da7d5ec0b75153d2a3f974804f741e852f24915
Yohan Putra Nugraha <nugraha.yohanp@gmail.com>
2020-11-10 15:16:14 +0700

    UI login page

--- client/src/pages/Login/Login.jsx
+++ client/src/pages/Login/Login.jsx
@@ -1,7 +1,10 @@
 import React, { Component, Fragment } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { Formik } from "formik";
+import Container from "@material-ui/core/Container";
 import Paper from "@material-ui/core/Paper";
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
 import "./Login.css";
 
 class Login extends Component {
@@ -11,9 +14,45 @@ class Login extends Component {
   }
   render() {
     return (
-      <Paper elevation={1} className="paper">
-        <h1>Login</h1>
-      </Paper>
+      <Container
+        fixed
+        style={{
+          height: "100vh",
+          display: "flex",
+          flexDirection: "row",
+          alignItems: "center",
+        }}
+      >
+        <Paper elevation={1} className="paper">
+          <h2 style={{ margin: "0" }}>EmpireCode Login</h2>
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            id="email"
+            className="textfield"
+            label="Email Address"
+            name="email"
+            autoComplete="email"
+            autoFocus
+          />
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="Password"
+            type="password"
+            id="password"
+            autoComplete="current-password"
+          />
+          <Button type="submit" fullWidth variant="contained" color="primary">
+            Sign In
+          </Button>
+        </Paper>
+      </Container>
     );
   }
 }
