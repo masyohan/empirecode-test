5e4f8f9c75122337a2557a29adb83d040a602b60
Yohan Putra Nugraha <nugraha.yohanp@gmail.com>
2020-11-10 22:17:21 +0700

    mysql config and create migrations setup

---
+++ server/migrations/setup.js
@@ -0,0 +1,15 @@
+const sql = require("../config/mysql");
+
+const query = `
+CREATE TABLE IF NOT EXISTS Users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    full_name VARCHAR(255) NOT NULL
+)
+`;
+
+sql.query(query, (err, result) => {
+  if (err) throw err;
+  console.log("Successfully created table Users");
+});
--- server/server.js
+++ server/server.js
@@ -2,12 +2,13 @@ const express = require("express");
 const server = express();
 const PORT = process.env.PORT || 4001;
 const cors = require("cors");
+const bodyParser = require("body-parser")
 const router = require("./routes");
 const errorHandler = require("./middlewares/errorHandler");
 
 server.use(cors());
-server.use(express.json());
-server.use(express.urlencoded({ extended: false}));
+server.use(bodyParser.json());
+server.use(bodyParser.urlencoded({ extended: true}));
 
 server.use(router);
 server.use(errorHandler);
